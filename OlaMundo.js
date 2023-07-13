dbed2c5abc140a2ec247b9d6a365dcc1892b4517
Max Willian <max-will-94@hotmail.com>
2023-07-13 20:15:16 -0300

    comandos para subir no git

--- OlaMundo.js
+++ OlaMundo.js
@@ -1,3 +1,6 @@
 console.log("Olá, Mundo!");
-console.log("Testando commit no vs code!")
+console.log("Testando commit no vs code!");
+console.log("git add .");
+console.log("git commit -m 'aqui a mensagem!');
+console.log("git push");
 
