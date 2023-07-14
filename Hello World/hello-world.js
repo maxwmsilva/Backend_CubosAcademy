52bfbb5af74276591f9464b665444cdab174beb4
Max Willian <max-will-94@hotmail.com>
2023-07-14 17:13:41 -0300

    subindo os arquivos apos o pull

--- Hello World/hello-world.js
+++ Hello World/hello-world.js
@@ -1,5 +1,7 @@
 console.log("hello world da cubos academy!")
 console.log("by Max Willian @maax.wm !")
 
-#testando git pull para baixar as modificações!
+// #testando git pull para baixar as modificações!
 
+console.log("git pull origin master")
+console.log("ou git pull")
