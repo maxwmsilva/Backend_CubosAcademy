f0eb67759bce488e62dd77ea1121569f545fd18c
MaxWillian <max-will-94@hotmail.com>
2023-07-19 11:55:53 -0300

    exemplo operadores logicos com not

---
+++ aulaCondiconais/exemplo1.js
@@ -0,0 +1,12 @@
+const temIngresso = true;
+const censura = 18;
+const idade = 20;
+const acompanhadoDosPais = true
+//ter ingresso e ter idade maior ou igual  a censura 
+if (!temIngresso === true) {
+  console.log(`variavem tem ingresso: ${temIngresso}pessoa sem ingresso`)}
+  if (idade >= censura || acompanhadoDosPais === true) {
+    console.log("Pode ver o filme")
+  }else {
+    console.log("Barrada, pilantra!")
+  }
