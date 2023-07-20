fe2dca9a1259209860ae256a69cbdcc69d83292f
MaxWillian <max-will-94@hotmail.com>
2023-07-19 21:49:39 -0300

    ex condicionais

---
+++ Arrays e loops/while.js
@@ -0,0 +1,7 @@
+const nomesDeEstudantes = ['jose','joao','maria', 'joana', 'andre'];
+
+let i = 0;
+while (i< nomesDeEstudantes.length) {
+  console.log(`${nomesDeEstudantes[i]}, indice ${i}`);
+  i++;
+}
