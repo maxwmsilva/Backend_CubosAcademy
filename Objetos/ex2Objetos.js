a1a25967ff3b415f0ad9c1b55d0905d59936df68
MaxWillian <max-will-94@hotmail.com>
2023-07-24 20:18:50 -0300

    ex2Objetos

---
+++ Objetos/ex2Objetos.js
@@ -0,0 +1,24 @@
+const cartao = {
+  Nome: "Max Willian",
+  Idade: 29,
+  ProdutosConsumidos: [
+    
+    {
+      Nome:"Cinema",
+      Preco: 30,
+      Quatidade: 2
+    },
+    {
+      Nome:"Deck",
+      Preco: 80,
+      Quatidade: 1
+    },
+    {
+      Nome:"BarDoJaci",
+      Preco: 40,
+      Quatidade: 1
+    }
+  ]
+};
+
+console.log(cartao)
