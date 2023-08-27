4c7eede789054ff74f23fe1ab23438f19183d7b5
MaxWillian <max-will-94@hotmail.com>
2023-08-27 19:22:50 -0300

    ex 6 desafio alternativo

--- Módulo_1/desafio-de-logica-alternativo-modulo-1-b2b-t07-dbe-ifood/6.Orientação/index.js
+++ Módulo_1/desafio-de-logica-alternativo-modulo-1-b2b-t07-dbe-ifood/6.Orientação/index.js
@@ -1,3 +1,25 @@
-function solucao(anotacaoAtleta, gabarito) {
-  
+function solucao(d, gabarito) {
+  let j = 0;
+  for (let i = 0; i < d.length; i++) {
+    if (d[i] === gabarito[j]) {
+      j++;
+    }
+  }
+  if (j === gabarito.length) {
+    console.log('PROVA COMPLETA');
+
+  } else {
+    console.log('PROVA INCOMPLETA');
+  }
+
 }
+
+solucao(["cavalo", "pedra", "tartaruga"],
+  ["cavalo", "pedra", "morcego"]);
+
+solucao(["lambari", "campanhia", "namoro"]
+["lambari", "campanhia", "namoro"]);
+
+
+solucao(["aviao", "cadeira", "caneco"],
+["pedra", "limpeza", "tigre", "macaco"]);
